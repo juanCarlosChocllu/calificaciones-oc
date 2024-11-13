ae0de56fd4d9186c67463a750d33c1732ddecf6c
Juan carlos chocllu <j.chocllu@opti-system.com>
2024-11-13 17:12:04 -0400

    varables de entorno

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     apps : [{
-      name: "api_analitic",
+      name: "api_calificaciones",
       script: "dist/main.js",
       instances: 1,
       log_date_format: "YYYY-MM-DD HH:mm Z", 
